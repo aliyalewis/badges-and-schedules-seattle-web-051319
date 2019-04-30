88f2974c7d4017716dcecad5f924ce51dde105d1
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-30 17:32:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,5 +6,6 @@ def batch_badge_creator(name_list)
   name_list.map {|names| badge_maker(names)}
 end
 
-def assign_rooms(speakers)
-  
+def assign_rooms(speaker, room_number)
+  "Hello, #{speaker}! You'll be assigned to room #{room_number}"
+end
